e249ef0db55f97fe98e6532bbc507da969b2b917
MarjaCarty <marja.d.carty@gmail.com>
2020-11-11 14:12:05 -0600

    Build update form and link it to movie list

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err.response));
+  };
+
+  const handleEdit = (e) => {
+    push(`/update-movie/${params.id}`);
   };
 
   const saveMovie = () => {
@@ -33,6 +38,7 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick={handleEdit}>Edit</button>
     </div>
   );
 }
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+
+const initialMovieValues = {
+  title: "",
+  director: "",
+  metascore: "",
+  stars: [],
+};
+
+export default function UpdateMovie() {
+  const [movieValues, setMovieValues] = useState(initialMovieValues);
+  const { id } = useParams();
+  const { push } = useHistory();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then((res) => {
+        setMovieValues(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const valueToUse =
+      name === "stars"
+        ? value.split(",")
+        : value || name === "metascore"
+        ? parseInt(value, 10)
+        : value;
+    setMovieValues({ ...movieValues, [name]: valueToUse });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        value={movieValues.title}
+        placeholder="Movie Title"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="director"
+        value={movieValues.director}
+        placeholder="Movie Director"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        name="metascore"
+        value={movieValues.metascore}
+        placeholder="Movie Metascore"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="stars"
+        value={movieValues.stars}
+        placeholder="Movie Stars"
+        onChange={handleChange}
+      />
+      <button type="submit">Update Movie</button>
+    </form>
+  );
+}
